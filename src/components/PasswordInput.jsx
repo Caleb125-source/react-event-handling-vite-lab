bbb3648e3eb461c227240c8b384a83a55702acc1
CalebMunyoki <munyokicaleb@gmail.com>
2026-01-17 18:21:30 +0300

    Add PasswordInput component with onChange handler

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,17 @@
 // Code PasswordInput Component Here
+import React from 'react';
+import styles from '../styles/PasswordInput.module.css';
 
 function PasswordInput (){
+    const handleChange = () => {
+        console.log("Entering password...");
+    };
     return(
-        <></>
-    )
+        <div>
+        <input className={styles.input} type="password" onChange={handleChange} placeholder="Enter your password" />
+        </div>
+    );
 }
+
 
 export default PasswordInput;
