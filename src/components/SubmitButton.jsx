600ad25a1e6532e45d505e9e260291f9b05725ab
CalebMunyoki <munyokicaleb@gmail.com>
2026-01-17 18:20:12 +0300

    Add SubmitButton component with mouse enter/leave handlers

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,18 @@
 // Code SubmitButton Component Here
+import React from 'react';
 
-function SubmitButton (){
-    return(
-        <></>
+function SubmitButton() {
+    const handleEnter = () => {
+        console.log("Mouse Entering");
+    }
+
+    const handleLeave = () => {
+        console.log("Mouse Exiting");
+    };
+    return (
+        <div>
+            <button type="submit" onMouseEnter={handleEnter} onMouseLeave={handleLeave} className="button">Submit Password</button>
+        </div>
     )
 }
 
